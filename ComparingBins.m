2169cc97e7480cc67e0dd58c6bfd15431f2a81ca
asvanschelt <32297141+asvanschelt@users.noreply.github.com>
2017-10-27 11:37:49 +0200

    Add files via upload

--- ComparingBins.m
+++ ComparingBins.m
@@ -1,7 +1,8 @@
-function [sort_nav, sort_fys, sort_nav_new, sort_fys_new, F_nav_new, bins_260_fys, bins_260_nav] = ComparingBins(F_nav, F_fys, time_total, gating_signal, params, k)
+function [F_nav_new, F_fys_new, bins_260_fys, bins_260_nav] = ComparingBins(F_nav, F_fys, F_MRI, gating_signal, params, k)
 
 %% Binning
 nBins = params.nBins;
+time_total = F_MRI(:,2);
 end_time = max(time_total);
 binning = params.sortingmethod;
 switch binning  
@@ -10,168 +11,115 @@ switch binning
     case 'v1'; p=5;
     case 'v2'; p=6;
     case 'v3'; p=7;
+    case 'b1'; p=8;
+    case 'b2'; p=9;
 end
 
+% Find the closest time value in the Nav or Fys signal which corresponds to
+% the MRI signal
 edges_nav = [-Inf, mean([F_nav(2:end,2) F_nav(1:end-1,2)],2)', +Inf];
-I_nav = discretize(time_total, edges_nav);
-
+I_nav = discretize(time_total, edges_nav); 
 edges_fys = [-Inf, mean([F_fys(2:end,2) F_fys(1:end-1,2)],2)', +Inf];
 I_fys = discretize(time_total, edges_fys);
 
 %% New FysLog and Nav signal with undersampling
 new_nav = F_nav(I_nav,1);
 new_nav_time = F_nav(I_nav,2);
-
 new_fys = F_fys(I_fys,1);
 new_fys_time = F_fys(I_fys,2);
 
 figure(1);
 subplot(211);
-plot(new_nav_time, new_nav);
-title('new navigator signal');
-
-figure(1)
+    plot(new_nav_time, new_nav);
+    title('new navigator signal');
 subplot(212);
-plot(new_fys_time,new_fys);
-title('new fyslog signal');
+    plot(new_fys_time,new_fys);
+    title('new fyslog signal');
 
 % Sort both signal in Bins 
-[F_nav_new] = SortInBins(new_nav,new_nav_time,params.nBins,1,p);
-[F_fys_new] = SortInBins(new_fys,new_fys_time,params.nBins,1,p);
-
-%% sort raw data
-
-
-
-%% Figures
-figure(1); hold on;
-subplot(211); hold on;
-E = zeros(nBins,1);
-C = hsv(nBins);
-    for iii=1:nBins
-        B = F_nav_new(:,6)==iii;
-        X = F_nav_new(B,1);
-        T = new_nav_time(B);
-        scatter(T,X,[],C(iii,:),'o');
-        axis([0 max(end_time) min(new_nav) max(new_nav)]);
-        E(iii,1)=length(find(F_nav_new(:,6)==iii));
-    end
-    hold off
-figure(1); hold on;
-subplot(212);
-E1 = zeros(nBins,1);
-    for ii=1:nBins
-        B1 = F_fys_new(:,6)==ii;
-        X1 = F_fys_new(B1,1);
-        T1 = new_fys_time(B1);
-        scatter(T1,X1,[],C(ii,:),'o');
-        axis([0 max(end_time) min(new_fys) max(new_fys)]);
-        E1(ii,1)=length(find(F_fys_new(:,6)==ii));
-    end
-    hold off
-
-% Set k_space in Bins
-for t=1:nBins
-    all = find(F_fys_new(:,6)==t);
-    none = find(F_nav_new(:,6)==t);
-    for a=1:length(all)
-    sort_nav_new(:,a,t)= k(:,a);
-    end
-    for b=1:length(none)
-    sort_fys_new(:,b,t)=k(:,b);
-    end
-    figure(2); title('FysLog new');
-    subplot(ceil(nBins/2),3,t);
-    plot(sort_nav_new(:,:,t));
-    figure(3); title('Navigator new');
-    subplot(ceil(nBins/2),3,t);
-    plot(sort_fys_new(:,:,t));
-end
+[F_nav_new] = SortInBins(new_nav,new_nav_time,params,1,p);
+[F_fys_new] = SortInBins(new_fys,new_fys_time,params,1,p);
 
 %% Sort k-space in Bins by FysLog
-bins = nan(1,260,71,8);
 bins_260_fys = nan(260,1);
 bins_260_nav = nan(260,1);
 bins_compare = nan(260,1);
 for i=1:260
-    bins(:,i,:,:) = F_nav(I_nav(i),p);
-    bins_260_fys(i) = F_fys(I_fys(i),p);
-    bins_260_nav(i) = F_nav(I_nav(i),p);
+    bins_260_fys(i) = F_fys(I_fys(i),3);
+    bins_260_nav(i) = F_nav(I_nav(i),3);
     if bins_260_fys(i) == bins_260_nav(i)
         bins_compare(i) = bins_260_fys(i);
     else
         bins_compare(i) = 0;
     end
-    
 end
 
-% Sort K trajectory
-for t1=1:params.nBins
-    all = find(bins_260_fys==t1);
-    none = find(bins_260_nav==t1);
-    for a1=1:length(all)
-    sort_nav(:,a1,t1)= k(:,a1);
-    end
-    for b1=1:length(none)
-    sort_fys(:,b1,t1)=k(:,b1);
-    end
-    figure(800); 
+% Trajectory plotting per Bin
+figure(10); hold on;
+for b=1:nBins
+    subplot(ceil((nBins/3)*2),3,b); hold on;
+    trajectories = find(bins_260_fys(:)==b);
+    plot(k(:,trajectories));
     title('FysLog');
-    subplot(ceil(params.nBins/2),3,t1);
-    plot(sort_nav(:,:,t1));
-    figure(801);
+    subplot(ceil((nBins/3)*2),3,nBins+b); hold on;
+    trajectories_nav = find(bins_260_nav(:)==b);
+    plot(k(:,trajectories_nav));
     title('Navigator');
-    subplot(ceil(params.nBins/2),3,t1);
-    plot(sort_fys(:,:,t1));
 end
+hold off;
 
+%% Figures
+% The Nav and Fys rebinned with undersampling according to the MRI signal
+figure(1); hold on;
+subplot(211); hold on;
+E = zeros(nBins,1);
+E1 = zeros(nBins,1);
+C = hsv(nBins);
+    for ii=1:nBins
+        B = F_nav_new(:,3)==ii;
+        X = F_nav_new(B,1);
+        T = new_nav_time(B);
+        subplot(211); hold on;
+        scatter(T,X,[],C(ii,:),'o');
+        axis([0 max(end_time) min(new_nav) max(new_nav)]);
+        E(ii,1)=length(find(F_nav_new(:,3)==ii));
+        
+        B1 = F_fys_new(:,3)==ii;
+        X1 = F_fys_new(B1,1);
+        T1 = new_fys_time(B1);
+        subplot(212);
+        scatter(T1,X1,[],C(ii,:),'o');
+        axis([0 max(end_time) min(new_fys) max(new_fys)]);
+        E1(ii,1)=length(find(F_fys_new(:,3)==ii));
+    end
+    hold off
 
-% FIGURES
+% FIGURE to plot the MRI signal binned to the different signals 
 figure(110); hold on;
 E = zeros(params.nBins,1);
+E1 = zeros(params.nBins,1);
 C = hsv(params.nBins);
 subplot(512); hold on;
-plot(time_total,gating_signal); hold on;
-title('Binning direct from k-space according to navigator')
-axis([0 end_time -inf inf]);
-for iv=1:params.nBins
-    B = bins_260_nav(:) == iv;
-    X = gating_signal(B);
-    T = time_total(B);
-    scatter(T,X,[],C(iv,:),'o'); hold on;
-    E(iv,1)=length(find(bins_260_nav==iv));
-end
-hold off;
-
-figure(110); hold on;
-E1 = zeros(params.nBins,1);
-C1 = hsv(params.nBins);
-subplot(514); hold on;
-plot(time_total,gating_signal); hold on;
-title('Binning direct from k-space according to FysLog')
-axis([0 end_time -inf inf]);
-for v=1:params.nBins
-    B1 = bins_260_fys(:) == v;
-    X1 = gating_signal(B1);
-    T1 = time_total(B1);
-    scatter(T1,X1,[],C1(v,:),'o'); hold on;
-    E1(v,1)=length(find(bins_260_fys==v));
-end
-hold off;
-
-figure(900); hold on;
-Ec = zeros(params.nBins,1);
-Cc = hsv(params.nBins);
-plot(time_total,gating_signal); hold on;
-    title('Comparing the binning')
+    plot(time_total,gating_signal); hold on;
+    title('Binning direct from k-space according to navigator')
     axis([0 end_time -inf inf]);
-for c=1:params.nBins
-    Bc = bins_compare(:) == c;
-    Xc = gating_signal(Bc);
-    Tc = time_total(Bc);
-    scatter(Tc,Xc,[],Cc(c,:),'o'); hold on;
-    Ec(c,1)=length(find(bins_compare==c));
+subplot(514); hold on;
+    plot(time_total,gating_signal); hold on;
+    title('Binning direct from k-space according to FysLog')
+    axis([0 end_time -inf inf]);
+for iii=1:params.nBins
+        B = bins_260_nav(:) == iii;
+        X = gating_signal(B);
+        T = time_total(B);
+        subplot(512); hold on;
+        scatter(T,X,[],C(iii,:),'o'); hold on;
+        E(iii,1)=length(find(bins_260_nav==iii));
+    subplot(514); hold on;
+        B1 = bins_260_fys(:) == iii;
+        X1 = gating_signal(B1);
+        T1 = time_total(B1);
+        scatter(T1,X1,[],C(iii,:),'o'); hold on;
+        E1(iii,1)=length(find(bins_260_fys==iii));
 end
-N = bins_compare(:) == 0;
-scatter(time_total(N), gating_signal(N), [],'ko'); hold on;
+hold off;
 end
--- ReadAll.m
+++ ReadAll.m
@@ -1,10 +1,12 @@
-function [F_nav, F_fys, F_MRI] = ReadAll(signal1, signal2, gating_signal_s, params)
+function [store_MRI, store_Fys, store_Nav, store_Fys_d, store_Nav_d] = ReadAll(signal1, signal2, gating_signal_s, params, gating_signal, k, raw_data_kspace)
 
+%% PARAMETERS
 % signal1 = 'SCANPHYSLOG_li_03102017_1804326_14_1_dce_spair_70ms_radial_navV4.log';
 % signal2 = 'devlogcurrent_1804-1807.log';
 % binning = 'v2';
 % nBins = 6;
 
+%% START CODE
 nBins = params.nBins;
 binning = params.sortingmethod;
 time_total = [0:2*params.time_blok:2*params.time_blok*260-params.time_blok];
@@ -12,9 +14,12 @@ time_total = [0:2*params.time_blok:2*params.time_blok*260-params.time_blok];
 [dcol_fys, t_fys] = ReadFysLog(signal1);
 [dcol_nav, t_nav] = ReadNav(signal2);
 
-dcol_nav(:) = dcol_nav(:)-mean(dcol_nav);
+% Equalize the signals
+dcol_nav(:) = dcol_nav(:)-mean(dcol_nav); %remove drift
 dcol_fys(:) = dcol_fys(:)/(max(dcol_fys)/max(dcol_nav));
+gating_signal_s(:) = gating_signal_s(:)/(max(gating_signal_s)/max(dcol_nav));
 
+% Align all signals
 if t_nav(length(t_nav))>t_fys(length(t_fys))
     diff = t_nav(length(t_nav))-t_fys(length(t_fys));
     t_fys(:) = t_fys(:)+diff;
@@ -31,12 +36,28 @@ switch binning
     case 'v1'; p=5;
     case 'v2'; p=6;
     case 'v3'; p=7;
+    case 'b1'; p=8;
+    case 'b2'; p=9;
 end
 
-[F_nav, peaks_nav, mins_nav] = SortInBins(dcol_nav,t_nav,nBins,1,p);
-[F_fys, peaks_fys, mins_fys] = SortInBins(dcol_fys,t_fys,nBins,500,p);
-[F_MRI, peaks, mns] = SortInBins(gating_signal_s,time_total,nBins,1,p);
-        
+% Sort each signal in Bins
+[F_nav, peaks_nav, mins_nav] = SortInBins(dcol_nav,t_nav,params,1,p);
+[F_fys, peaks_fys, mins_fys] = SortInBins(dcol_fys,t_fys,params,500,p);
+[F_MRI, peaks_MRI, mns_MRI] = SortInBins(gating_signal_s,time_total,params,1,p);
+
+% Translate both the fys and nav to MRI signal for reconstruction
+[F_nav_new, F_fys_new, bins_260_fys, bins_260_nav] = ComparingBins(F_nav, F_fys, F_MRI, gating_signal, params, k);
+
+% Sort the raw data in the bins defined by the different methods
+[store_MRI] = TrySortRawK(F_MRI(:,3), raw_data_kspace, params);
+[store_Nav] = TrySortRawK(F_nav_new(:,3), raw_data_kspace, params);
+[store_Fys] = TrySortRawK(F_fys_new(:,3), raw_data_kspace, params);
+[store_Fys_d] = TrySortRawK(bins_260_fys, raw_data_kspace, params);
+[store_Nav_d] = TrySortRawK(bins_260_nav, raw_data_kspace, params);
+
+
+%% Figures
+% Figure with the different binning methods in subplots
 figure(110); hold on;
 E = zeros(nBins,1);
 E1 = zeros(nBins,1);
@@ -45,41 +66,41 @@ C = hsv(nBins);
 subplot(511);
     plot(t_nav, dcol_nav); hold on;
     title('Navigator Binning')
+subplot(513);
+    plot(t_fys, dcol_fys); hold on;
+    title('FysLog Binning')
+subplot(515);hold on;
+    plot(time_total, gating_signal_s); hold on;
+    axis([0 max(time_total) -inf inf]);
+    title('MRI Binning')
     for i=1:nBins
-        B = F_nav(:,p)==i;
+        subplot(511);
+        B = F_nav(:,3)==i;
         X = F_nav(B,1);
         T = t_nav(B);
         scatter(T,X,[],C(i,:),'o');
         axis([0 max(t_nav) min(dcol_nav) max(dcol_nav)]);
-        E(i,1)=length(find(F_nav(:,p)==i));
-    end
-    hold off
-subplot(513);
-    plot(t_fys, dcol_fys); hold on;
-    title('FysLog Binning')
-    for ii=1:nBins
-        B1 = F_fys(:,p)==ii;
+        E(i,1)=length(find(F_nav(:,3)==i));
+        
+        subplot(513);
+        B1 = F_fys(:,3)==i;
         X1 = F_fys(B1,1);
         T1 = t_fys(B1);
-        scatter(T1,X1,[],C(ii,:),'.');
+        scatter(T1,X1,[],C(i,:),'.');
         axis([0 max(t_fys) min(dcol_fys) max(dcol_fys)]);
-        E1(ii,1)=length(find(F_nav(:,p)==ii));
+        E1(i,1)=length(find(F_nav(:,3)==i));
+        
+        subplot(515);
+        B3 = F_MRI(:,3)==i;
+        X3 = F_MRI(B3,1);
+        T3 = time_total(B3);
+        scatter(T3,X3,[],C(i,:),'o');
+        E3(i,1)=length(find(F_MRI(:,3)==i));
     end
     hold off
-subplot(515);hold on;
-plot(time_total, gating_signal_s); hold on;
-axis([0 max(time_total) -inf inf]);
-title('MRI Binning')
-for iii=1:nBins
-    B3 = F_MRI(:,p)==iii;
-    X3 = F_MRI(B3,1);
-    T3 = time_total(B3);
-    scatter(T3,X3,[],C(iii,:),'o');
-    E3(iii,1)=length(find(F_MRI(:,p)==iii));
-end
-hold off
 
 
+% Figure with all 3 different signals in one with the peaks and mins
 figure(150); hold on;
 plot(t_nav,dcol_nav,'r'); hold on;
 scatter(peaks_nav(:,1),peaks_nav(:,2), 'r*'); hold on;
@@ -87,8 +108,8 @@ scatter(mins_nav(:,1),mins_nav(:,2), 'r*'); hold on;
 plot(t_fys,dcol_fys,'b'); hold on;
 scatter(peaks_fys(:,1),peaks_fys(:,2), 'b*'); hold on;
 scatter(mins_fys(:,1),mins_fys(:,2), 'b*'); hold on;
-plot(time_total,F_MRI(:,1)*100,'g'); hold on;
-scatter(peaks(:,1),peaks(:,2)*100, 'g*'); hold on;
-scatter(mns(:,1),mns(:,2)*100, 'g*'); hold off;
+plot(time_total,F_MRI(:,1),'g'); hold on;
+scatter(peaks_MRI(:,1),peaks_MRI(:,2), 'g*'); hold on;
+scatter(mns_MRI(:,1),mns_MRI(:,2), 'g*'); hold off;
     legend('Navigator','Peaks navigator','','Fyslog', 'Peaks fyslog','','MRI', 'Peaks MRI','');
 end
